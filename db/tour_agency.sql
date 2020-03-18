d8ba8f2a144835e70e1dbdab382f72d640cff8da
junwu <junwu2012@hotmail.com>
2020-03-18 21:29:26 +0000

    add sql function

--- db/tour_agency.sql
+++ db/tour_agency.sql
@@ -36,3 +36,25 @@ CREATE TABLE bookings (
   tourist_id INT REFERENCES tourists(id) ON DELETE CASCADE,
   booking_date DATE
 );
+
+DROP FUNCTION get_price(tour_id integer)
+CREATE OR REPLACE FUNCTION get_price(tour_id integer) RETURNS table(price integer)
+as
+$$
+ SELECT price from tours where id = tour_id;
+$$
+LANGUAGE sql;
+
+DROP FUNCTION Booking(tourist_id INT,tour_id INT,booking_date DATE);
+CREATE FUNCTION Booking(tourist_id INT,tour_id INT,booking_date DATE) RETURNS integer AS $$
+DECLARE
+   booking_id integer;
+begin
+UPDATE tourists SET funds = funds - (select get_price(tour_id)) where id = tourist_id;
+
+INSERT INTO bookings(tourist_id,tour_id,booking_date) values (tourist_id,tour_id,booking_date) returning id into booking_id;
+return booking_id;
+end;
+
+$$
+LANGUAGE plpgsql;
